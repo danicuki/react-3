bf921147cfb199fa14dd771eee859fd97ed6d950
Daniel Cukier <danicuki@gmail.com>
2018-10-18 13:39:31 -0300

    syntax improvements, tags vs functions

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
-import Card from "./Card.js";
-import ArtistCard from "./ArtistCard.js";
+import Card from './Card.js';
+import ArtistCard from './ArtistCard.js';
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
 
     this.state = {
-      artists: []
+      artists:[]
     };
 
     this.handleClick = this.handleClick.bind(this);
+
   }
+  componentDidMount(){
+    const BASE_URL = 'https://peaceful-badlands-98440.herokuapp.com';
+    const BASE_OPTIONS = {
 
-  componentDidMount() {
-    const BASE_URL = "https://peaceful-badlands-98440.herokuapp.com"
+    }
     const options = {
-      method: "post",
-      headers: {
-        'Content-Type': 'application/json'
-      },
+      method: 'post',
+      headers:{'Content-Type': 'application/json'},
       credentials: 'include',
-      body: JSON.stringify({ email: "rafael@laboratoria.la", password: "banana" })
+      body: JSON.stringify({email: 'rafael@laboratoria.la', password: 'banana'})
     };
-
     fetch(`${BASE_URL}/login`, options)
+      // .then(data => console.log(data));
       .then(res => {
         const options = {
-          method: "get",
-          credentials: 'include'
+          method: 'get',
+          credentials: 'include',
         };
-
         fetch(`${BASE_URL}/artists`, options)
           .then(res => res.json())
           .then(data => this.setState({ artists: data }));
-        });
+      })
   }
-
-  handleClick() {
+  handleClick(){
   }
-
   render() {
     return (
       <div>
-        {this.state.artists.map((artist) => 
-          <ArtistCard name={artist.name} genre={artist.genre} id={artist.id} />
-        )}
-        
-        <Card>
-          <button onClick={this.handleClick}>Pegar os artistas</button>
-        </Card>
+        {this.state.artists.map(artist => <ArtistCard {...artist} key={artist.id} />)}
       </div>
     );
   }
